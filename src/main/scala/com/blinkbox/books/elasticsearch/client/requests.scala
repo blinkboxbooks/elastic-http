74f374b1fe3663c279a82588488eff7ea05598c5
Aldo Stracquadanio <aldos@blinkbox.com>
2014-12-16 13:41:05 +0000

    Better arrange implicits, implement search req/resp

--- src/main/scala/com/blinkbox/books/elasticsearch/client/requests.scala
+++ src/main/scala/com/blinkbox/books/elasticsearch/client/requests.scala
@@ -1,46 +1,106 @@
 package com.blinkbox.books.elasticsearch.client
 
-import com.sksamuel.elastic4s.{IndexDefinition, GetDefinition}
+import com.sksamuel.elastic4s.{ IndexDefinition, GetDefinition }
+import com.sksamuel.elastic4s.ElasticDsl.SearchDefinition
+import org.elasticsearch.action.get.GetRequest
+import org.elasticsearch.action.index.IndexRequest
+import org.elasticsearch.action.search.SearchRequest
 import org.json4s.JValue
-import spray.http.HttpRequest
+import spray.http.{ Uri, HttpRequest }
 import spray.client.pipelining._
 import spray.httpx.unmarshalling.FromResponseUnmarshaller
 
-trait ClientRequests {
-  implicit object GetElasticRequest extends ElasticRequest[GetDefinition, GetResponse[JValue]] {
-    override def request(req: GetDefinition): HttpRequest = {
+trait GetSupport {
+  sealed trait GetBase {
+    def urlFor(request: GetRequest) = s"/${request.index}/${request.`type`}/${request.id}"
+
+    def fieldsFor(request: GetRequest): Map[String, String] = Option(request.fields).
+      fold(Map.empty[String, String])(f => Map("fields" -> f.mkString(",")))
+
+    def paramsFor(request: GetRequest): Map[String, String] = Map(
+      "realtime" -> request.realtime.toString,
+      "ignoreErrorsOnGeneratedFields" -> request.ignoreErrorsOnGeneratedFields.toString,
+      "routing" -> request.routing,
+      "preference" -> request.preference,
+      "refresh" -> request.refresh.toString,
+      "version" -> request.version.toString,
+      "versionType" -> request.versionType.toString
+    ) ++ fieldsFor(request)
+
+    def requestFor(req: GetDefinition) = {
       val builtRequest = req.build
       Get(s"/${builtRequest.index}/${builtRequest.`type`}/${builtRequest.id}")
     }
   }
 
-  implicit object IndexElasticRequest extends ElasticRequest[IndexDefinition, IndexResponse] {
-    override def request(req: IndexDefinition): HttpRequest = {
-      val builtRequest = req.build
-      Put(s"/${builtRequest.index}/${builtRequest.`type`}/${builtRequest.id}", req._fieldsAsXContent.string)
-    }
+  implicit object GetElasticRequest extends ElasticRequest[GetDefinition, GetResponse[JValue]] with GetBase {
+    override def request(req: GetDefinition): HttpRequest = requestFor(req)
   }
-}
 
-trait TypedClientRequests {
   case class TypedGetDefinition[T: FromResponseUnmarshaller](req: GetDefinition)
 
   implicit class TypedGetDefinitionOps(req: GetDefinition) {
     def sourceIs[T: FromResponseUnmarshaller] = TypedGetDefinition[T](req)
   }
 
-  class TypedGetElasticRequest[T: FromResponseUnmarshaller] extends ElasticRequest[TypedGetDefinition[T], GetResponse[T]] {
-    override def request(typedReq: TypedGetDefinition[T]): HttpRequest = {
-      val builtRequest = typedReq.req.build
-      Get(s"/${builtRequest.index}/${builtRequest.`type`}/${builtRequest.id}")
-    }
+  class TypedGetElasticRequest[T: FromResponseUnmarshaller]
+    extends ElasticRequest[TypedGetDefinition[T], GetResponse[T]] with GetBase {
+
+    override def request(typedReq: TypedGetDefinition[T]): HttpRequest = requestFor(typedReq.req)
   }
 
   import scala.language.implicitConversions
 
-  implicit def toTypedGetElasticRequest[T](implicit marshaller: FromResponseUnmarshaller[T]): TypedGetElasticRequest[T] = new TypedGetElasticRequest[T]
+  implicit def toTypedGetElasticRequest[T](implicit marshaller: FromResponseUnmarshaller[T]): TypedGetElasticRequest[T] =
+    new TypedGetElasticRequest[T]
+}
+
+trait IndexSupport {
+  implicit object IndexElasticRequest extends ElasticRequest[IndexDefinition, IndexResponse] {
+    def maybeSegment(param: String) = Option(param).fold("")(p => s"/$p")
+
+    def urlFor(req: IndexRequest) = s"/${req.index}/${req.`type`}${maybeSegment(req.id)}"
+
+    def paramsFor(request: IndexRequest): Map[String, String] = Map(
+      "ttl" -> request.ttl.toString,
+      "versionType" -> request.versionType.toString,
+      "version" -> request.version.toString,
+      "refresh" -> request.refresh.toString,
+      "timeout" -> request.timeout.toString,
+      "replicationType" -> request.replicationType.toString,
+      "consistencyLevel" -> request.consistencyLevel.toString,
+      "opType" -> request.opType.toString,
+      "timestamp" -> request.timestamp,
+      "parent" -> request.parent,
+      "routing" -> request.routing
+    )
+
+    override def request(req: IndexDefinition): HttpRequest = {
+      val builtRequest = req.build
+      val uri = Uri(urlFor(builtRequest)).withQuery(paramsFor(builtRequest))
+
+      Put(uri, req._fieldsAsXContent.string)
+    }
+  }
+}
+
+trait SearchSupport {
+  implicit object SearchElasticRequest extends ElasticRequest[SearchDefinition, SearchResponse[JValue]] {
+    def urlFor(req: SearchRequest) = s"/${req.indices.mkString(",")}/${req.types.mkString(",")}/_search"
+
+    def paramsFor(request: SearchRequest): Map[String, String] =
+      Option(request.routing).fold(Map.empty[String, String])(r => Map("routing" -> r))
+
+    override def request(req: SearchDefinition): HttpRequest = {
+      val builtRequest = req.build
+      val uri = Uri(urlFor(builtRequest)).withQuery(paramsFor(builtRequest))
+
+      Post(uri, req._builder.toString)
+    }
+  }
 }
 
 object SprayElasticClientRequests
-  extends ClientRequests
-  with TypedClientRequests
+  extends IndexSupport
+  with SearchSupport
+  with GetSupport
--- src/main/scala/com/blinkbox/books/elasticsearch/client/responses.scala
+++ src/main/scala/com/blinkbox/books/elasticsearch/client/responses.scala
@@ -8,4 +8,12 @@ case class GetResponse[T](
     _version: Option[Long],
     _source: Option[T])
 
+case class ShardsStats(total: Int, successful: Int, failed: Int)
+case class SearchHit[T](_index: String, _type: String, _id: Int, _source: T)
+case class SearchHits[T](total: Int, hits: Array[SearchHit[T]])
+case class SearchResponse[T](
+  _shards: ShardsStats,
+  hits: SearchHits[T]
+)
+
 case class IndexResponse(_index: String, _type: String, _id: String, _version: Long, created: Boolean)
